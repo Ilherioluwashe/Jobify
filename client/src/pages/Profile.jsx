5afbd3328c5399b23c3a3bc982fb9439fb9ad897
Michael Akingbade <akingbademichael7@gmail.com>
2024-06-25 10:54:08 +0100

    queries update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,25 +1,29 @@
 import { FormRow, SubmitBtn } from '../components'
 import Wrapper from '../assets/wrappers/DashboardFormPage'
-import { useOutletContext } from 'react-router-dom'
+import { redirect, useOutletContext } from 'react-router-dom'
 import { Form } from 'react-router-dom'
 import customFetch from '../utils/customFetch'
 import { toast } from 'react-toastify'
 
-export const action = async ({ request }) => {
-  const formData = await request.formData()
-  const file = formData.get('avatar')
-  if (file && file.size > 500000) {
-    toast.error('Image sze too large')
-    return null
+export const action =
+  (queryClient) =>
+  async ({ request }) => {
+    const formData = await request.formData()
+    const file = formData.get('avatar')
+    if (file && file.size > 500000) {
+      toast.error('Image sze too large')
+      return null
+    }
+    try {
+      await customFetch.patch('/users/update-user', formData)
+      queryClient.invalidateQueries(['user'])
+      toast.success('Profile updated Successfully')
+      return redirect('/dashboard')
+    } catch (error) {
+      toast.error(error?.response?.data?.msg)
+      return null
+    }
   }
-  try {
-    await customFetch.patch('/users/update-user', formData)
-    toast.success('Profile updated Successfully')
-  } catch (error) {
-    toast.error(error?.response?.data?.msg)
-  }
-  return null
-}
 
 const Profile = () => {
   const { user } = useOutletContext()
